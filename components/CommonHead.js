80a6d7c1e7e2a1ae7f3edf699e6bba8df3cc361e
BJTiew <132333550+BJTiew@users.noreply.github.com>
2023-08-28 22:32:21 +0800

    Update CommonHead.js

--- components/CommonHead.js
+++ components/CommonHead.js
@@ -29,19 +29,7 @@ const CommonHead = ({ meta, children }) => {
                 />
             )}
             {BLOG.SEO_BAIDU_SITE_VERIFICATION && (<meta name="baidu-site-verification" content={BLOG.SEO_BAIDU_SITE_VERIFICATION} />)}
-            {BLOG.CLARITY_TRACKING_ID && (
-                <script
-                    dangerouslySetInnerHTML={{
-                        __html: `
-                        (function(c,l,a,r,i,t,y){
-                            c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
-                            t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
-                            y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
-                        })(window, document, "clarity", "script", "${BLOG.CLARITY_TRACKING_ID}")";
-                        `
-                    }}
-                />
-            )}
+            <ClarityScript />
             <meta name="keywords" content={keywords} />
             <meta name="description" content={description} />
             <meta property="og:locale" content={lang} />

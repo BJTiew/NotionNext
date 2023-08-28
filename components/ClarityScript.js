fc3773cd910e23343839fcb2b894fc54f711a838
BJTiew <132333550+BJTiew@users.noreply.github.com>
2023-08-28 22:37:36 +0800

    Update ClarityScript.js

--- components/ClarityScript.js
+++ components/ClarityScript.js
@@ -1,4 +1,4 @@
-import BLOG from '@/blog.config'
+import BLOG from '@/blog.config';
 
 const ClarityScript = () => (
   <script
@@ -8,8 +8,8 @@ const ClarityScript = () => (
           c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
           t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
           y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
-        })(window, document, "clarity", "script", "${BLOG.CLARITY_TRACKING_ID}");
-      `,
+        })(window, document, "clarity", "script", "${BLOG.CLARITY_TRACKING_ID}")
+      `
     }}
   />
 );
